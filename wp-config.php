eb16a6711ab75c5ee6485dcfd7c09b29dbed813c
mamadou jallow <sulsiralumo@gmail.com>
2015-05-03 17:19:56 +0000

    bring in the . files for hidding and atributes

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'moherst_update');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'shs123');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
